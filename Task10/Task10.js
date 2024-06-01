e7c5ac7fdc743145f363b37a3bed3bcfad46b7b2
ABDALLAH ALABDELAZIZ <129231021+Abdullahshade@users.noreply.github.com>
2024-06-01 15:15:38 +0300

    Task10

--- Task10/Task10.js
+++ Task10/Task10.js
@@ -4,9 +4,8 @@ function GetIP() {
     fetch("https://api.ipify.org?format=json")
     .then(response => response.json())  
     .then(data => {
-        const ip=data.ip;
-        
-        document.getElementById("GetIP").textContent=data.ip;
+        const IP=data.ip;
+        document.getElementById("GetIP").textContent=IP;
     })
     .catch(error => {
         console.error('Error fetching the IP address:', error); 
